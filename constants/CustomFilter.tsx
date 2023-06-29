73d5b25718c66811ca5a6bd8542ee2d70a068a1a
saul <saul@atomrigs.io>
2023-06-29 19:32:37 +0900

    필터 컴포넌트

---
+++ constants/CustomFilter.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import React, { useState, Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+import { updateSearchParams } from '@/utils';
+
+type CustomFilterProps = {
+  title: string,
+  options: { title: string, value: string }[],
+};
+
+export default function CustomFilter({ title, options }: CustomFilterProps) {
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string, value: string }) => {
+    // URL search param 업데이트:
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+    // 새로운 URL로 이동:
+    router.push(newPathName);
+  };
+
+  return (
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={e => {
+          setSelected(e); // Update the selected option in state
+          handleUpdateParams(e); // Update the URL search parameters and navigate to the new URL
+        }}
+      >
+        <div className="relative w-fit z-10">
+          {/* Button for the listbox */}
+          <Listbox.Button className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron_up-down"
+            />
+          </Listbox.Button>
+          {/* Transition for displaying the options */}
+          <Transition
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {/* Map over the options and display them as listbox options */}
+              {options.map(option => (
+                <Listbox.Option
+                  key={option.title}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                    }`
+                  }
+                  value={option}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
